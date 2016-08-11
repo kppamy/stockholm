05674697f42c236f553c41897f6bb4db756e6a54
kppamy <zhhcay@163.com>
2016-08-11 10:28:02 +0800

    add bascis ma mean calculate and so

--- pydata/strategy.py
+++ pydata/strategy.py
@@ -6,15 +6,10 @@ class Strategy(object):
 
     def mark_single_quote(self,quote):
             df=quote
-            df.loc[(df.Volume>0)&(df.Volume >df.shift(1).Volume) & (df.Adj_Close > df.shift(1).Adj_Close),'mark']=2
-            df.loc[(df.Volume>0)&(df.Volume >df.shift(1).Volume) & (df.Adj_Close < df.shift(1).Adj_Close),'mark']=-2
-            df.loc[(df.Volume>0)&(df.Volume >df.shift(1).Volume) & (df.Adj_Close == df.shift(1).Adj_Close),'mark']=1
-            df.loc[(df.Volume>0)&(df.Volume == df.shift(1).Volume) & (df.Adj_Close >df.shift(1).Adj_Close),'mark']=1
-            df.loc[(df.Volume>0)&(df.Volume == df.shift(1).Volume) & (df.Adj_Close < df.shift(1).Adj_Close),'mark']= -1
-            df.loc[(df.Volume>0)&(df.Volume == df.shift(1).Volume) & (df.Adj_Close == df.shift(1).Adj_Close),'mark']= 0
-            df.loc[(df.Volume>0)&(df.Volume < df.shift(1).Volume) & (df.Adj_Close > df.shift(1).Adj_Close),'mark']=1
-            df.loc[(df.Volume>0)&(df.Volume < df.shift(1).Volume) & (df.Adj_Close < df.shift(1).Adj_Close),'mark']=-1
-            df.loc[(df.Volume>0)&(df.Volume < df.shift(1).Volume) & (df.Adj_Close == df.shift(1).Adj_Close),'mark']=-1
+            df.loc[(df.v_change>0) & (df.p_change>0), 'mark']=2
+            df.loc[(df.v_change<0) & (df.p_change>0), 'mark']=1
+            df.loc[(df.v_change<0) & (df.p_change<0), 'mark']=-1
+            df.loc[(df.v_change>0) & (df.p_change<0), 'mark']=-2
             
     def mark_all_down(self, all_quotes):
         start = timeit.default_timer()
@@ -38,5 +33,35 @@ class Strategy(object):
         pd.DataFrame.to_csv(self.all_marks,'allmarks.csv')
         print("export is complete... time cost: " + str(round(timeit.default_timer() - start)) + "s" + "\n")
 
+    def ma_cal(self,df):
+        prc=df.Close
+        df['ma5']=prc.rolling(5).mean()
+        df['ma10']=prc.rolling(10).mean()
+        df['ma20']=prc.rolling(20).mean()
+
+    def basics_cal(self,all_quotes):
+        start = timeit.default_timer()
+        st=all_quotes
+        st.columns=['name', 'Symbol', 'Adj_Close', 'Close', 'Date','High','Low', 'Open', 'symbol', 'Volume']
+        st=st.drop('symbol',axis=1)
+        symbols=st[['name', 'Symbol']]
+        symbols.drop_duplicates(inplace=True)
+        print('all symbols : \n',symbols.head())
+        count=0
+        for i in symbols[:4]:
+            count=count+1
+            print("process ",i)
+            df=st[st.Symbol == i]
+            v=df.Volume
+            df['v_change']=v.pct_change()
+            prc=df.Close
+            df['p_change']=prc.pct_change()
+            self.ma_cal(df)
+            self.mark_single_quote(df)
+            if(count==1):
+                self.all_marks=df
+            else:
+                self.all_marks=self.all_marks.append(df)
+        print("basics data process",all_quotes.head())
     def run(self,all_quotes):
         self.mark_all_down(all_quotes)

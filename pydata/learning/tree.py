23ec83489ae4f99825e41974813ad6884fa6a7a9
chenay <anyan.chen@technicolor.com>
2020-09-15 09:37:42 +0800

    always consider the empty case
    1. when use recursion to fill the output vector,
        or build nodes from sorted array, always take
        care that the start is start, not 0.....
    2. use condition check before entering the recuring
    function to decrease the recursion depth as much as
    possible
    
    sorted array <------> BST: in order traverse
    print BST: pre order traverse
    
    tooooooooo  inefficient
    
    TODO: use same idea to do sortedArrayToBST as printBT

--- pydata/learning/tree.py
+++ pydata/learning/tree.py
@@ -17,6 +17,7 @@ class Tree:
     def buildNode(self, nums, start, end):
         if start == end:
             return TreeNode(nums[start])
+        # mid = (end + start) // 2 +1  why wrong?  if odd numbers input, then it's not in the middle
         mid = (end + start) // 2
         root = TreeNode(nums[mid])
         if start <= mid - 1:
@@ -28,10 +29,15 @@ class Tree:
         return root
 
     # 108. Convert Sorted Array to Binary Search Tree
+    # Runtime: 120 ms, faster than 12.95% of Python3 online submissions for Convert Sorted Array to Binary Search Tree.
+    # Memory Usage: 16.1 MB, less than 59.31% of Python3 online submissions for Convert Sorted Array to Binary Search Tree.
     def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        if not bool(nums):
+            return None
         return self.buildNode(nums, 0, len(nums) - 1)
 
     def generate_sortedArray(self):
+        arr00 = []
         arr0 = [-3, -2, -1, 3, 4, 5, 7, 8]
         arr1 = [-10, -3, 0, 5, 9]
         arr2 = sorted(random.random_integers(-3, 10, 8))
@@ -44,19 +50,21 @@ class Tree:
             [1, 2, 2, 3, 3, -1, -1, 4, 4],
             [10, 5, 15, -1, -1, 6, 20]
         ]
-        cases = [arr0, arr1, arr2, arr3]
+        cases = [arr00, arr0, arr1, arr2, arr3]
         cases.extend(arr4)
         return cases
 
     # 655. Print Binary Tree
+    # Runtime: 40 ms, faster than 29.39% of Python3 online submissions for Print Binary Tree.
+    # Memory Usage: 13.7 MB, less than 89.80% of Python3 online submissions for Print Binary Tree.
     def printTree(self, root: TreeNode) -> List[List[str]]:
-        height = self.getHeight(root)
+        height = self.maxDepth(root)
         wid = 2 ** height - 1
         nodes = [['   ' for col in range(wid)] for row in range(height)]
         self.fill(root, nodes, 0, wid - 1, 1)
         rows = [reduce(lambda x, y: x + y, nodes[i]) for i in range(height)]
         for i, item in enumerate(rows):
-            print(item,sep=None)
+            print(item, sep=None)
         return nodes
 
     def fill(self, root: TreeNode, nodes: List[List[str]], start: int, end: int, height: int):
@@ -67,21 +75,23 @@ class Tree:
             return
         pos = (start + end) // 2
         nodes[height - 1][pos] = str(root.val)
-        if pos - 1 >= 0:
-            self.fill(root.left, nodes, 0, pos - 1, height + 1)
-        if pos + 1 <= end:
+        if pos - 1 >= 0 and root.left:
+            self.fill(root.left, nodes, start, pos - 1, height + 1)
+        if pos + 1 <= end and root.right:
             self.fill(root.right, nodes, pos + 1, end, height + 1)
 
     # 104. Maximum Depth of Binary Tree
-    def getHeight(self, root: TreeNode) -> int:
+    # Runtime: 44 ms, faster than 61.37% of Python3 online submissions for Maximum Depth of Binary Tree.
+    # Memory Usage: 15.4 MB, less than 42.18% of Python3 online submissions for Maximum Depth of Binary Tree.
+    def maxDepth(self, root: TreeNode) -> int:
         if not root:
             return 0
         left = 0
         if root.left:
-            left = self.getHeight(root.left)
+            left = self.maxDepth(root.left)
         right = 0
         if root.right:
-            right = self.getHeight(root.right)
+            right = self.maxDepth(root.right)
         return max(left, right) + 1
 
     def format_test(self, func, print_func=None):
